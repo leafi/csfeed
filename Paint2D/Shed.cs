4b1e05336be1a47cdbf5b49a40a54ae488528793
leafi <leaf@leaf.sx>
2017-06-04 00:53:01 +0100

    bleh.. for later.

---
+++ Paint2D/Shed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using SharpBgfx;
+
+namespace Csfeed
+{
+    public abstract class Shed
+    {
+		protected SharpBgfx.Program loadScx(string name)
+		{
+			var vs = new Shader(MemoryBlock.FromArray<byte>(File.ReadAllBytes($"../../shaders/bin/glsl/{name}.scx.vshader")));
+			var fs = new Shader(MemoryBlock.FromArray<byte>(File.ReadAllBytes($"../../shaders/bin/glsl/{name}.scx.fshader")));
+
+		}
+    }
+}
