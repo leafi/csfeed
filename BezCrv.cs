61dc80031c555609647772ab6dc803891dea5701
leafi <leaf@leaf.sx>
2017-06-04 19:57:24 +0100

    Use helpers in BezCrv

--- BezCrv.cs
+++ BezCrv.cs
@@ -9,48 +9,15 @@ namespace Csfeed
 {
     public static class BezCrv
     {
-		private static Matrix4x4 projectionMatrix;
-		private static Matrix4x4 viewMatrix = Matrix4x4.Identity;
-		private static Matrix4x4 identityMatrix = Matrix4x4.Identity;
-
 		const byte VIEW_ID = 1;
-
-		private static SharpBgfx.Program colorShader;
-		private static VertexLayout vertexLayout;
 
 		public static void Load()
 		{
-			var vs = new Shader(MemoryBlock.FromArray<byte>(File.ReadAllBytes("../../shaders/bin/glsl/color.scx.vshader")));
-			var fs = new Shader(MemoryBlock.FromArray<byte>(File.ReadAllBytes("../../shaders/bin/glsl/color.scx.fshader")));
-			colorShader = new SharpBgfx.Program(vs, fs, true);
-
-			vertexLayout = new VertexLayout();
-			vertexLayout.Begin();
-			vertexLayout.Add(VertexAttributeUsage.Position, 2, VertexAttributeType.Float);
-			vertexLayout.Add(VertexAttributeUsage.Color0, 4, VertexAttributeType.Float);
-			vertexLayout.End();
 		}
 
 		private unsafe static void putVertex2f(TVBFloat tvb, float x, float y, Vector4 color)
 		{
-			if (tvb.vidx >= TVBFloat.MAX_VERTS) {
-				throw new Exception("too many verts");
-			}
-
-			*tvb.vptr = x;
-			tvb.vptr++;
-			*tvb.vptr = y;
-			tvb.vptr++;
-			*tvb.vptr = color.X;
-			tvb.vptr++;
-			*tvb.vptr = color.Y;
-			tvb.vptr++;
-			*tvb.vptr = color.Z;
-			tvb.vptr++;
-			*tvb.vptr = color.W;
-			tvb.vptr++;
-
-			tvb.vidx++;
+			Sheds.Color.PutVertexXYColor(tvb, x, y, color);
 		}
 
 		private static void putLine(TVBFloat tvb, Vector2 a, Vector2 b, float width, Vector4 color)
@@ -98,20 +65,10 @@ namespace Csfeed
 		{
 			var engine = Program.Engine;
 
-			projectionMatrix = Matrix4x4.CreateOrthographicOffCenter(0f, engine.Width, engine.Height, 0f, 0f, 2f);
-
-			Bgfx.SetViewSequential(VIEW_ID, true);
+			ViewHelper.PrepView2DNative(VIEW_ID);
 			Bgfx.SetViewClear(VIEW_ID, ClearTargets.Color | ClearTargets.Depth, 0x232729ff, 1f, 0);
 
-			Bgfx.SetViewRect(VIEW_ID, 0, 0, engine.Width, engine.Height);
-
-			fixed (float* pProj = &(projectionMatrix.M11)) {
-				fixed (float* pView = &(viewMatrix.M11)) {
-					Bgfx.SetViewTransform(VIEW_ID, pView, pProj);
-				}
-			}
-
-			var tvb = new TVBFloat(vertexLayout);
+			var tvb = new TVBFloat(Sheds.Color.VertexLayout);
 			putVertex2f(tvb, 50f, 50f, new Vector4(1f, 0f, 0f, 1f));
 			putVertex2f(tvb, 50f, 150f, new Vector4(0f, 1f, 0f, 1f));
 			putVertex2f(tvb, 150f, 150f, new Vector4(0f, 0f, 1f, 1f));
@@ -125,15 +82,7 @@ namespace Csfeed
 
 			putQuadCurve(tvb, new Vector2(300f, 100f), new Vector2(500f, 300f), new Vector2(300f, 500f), 10, 20f, new Vector4(1f, 1f, 1f, 1f));
 
-
-
-			fixed (float* pMdl = &(identityMatrix.M11)) {
-				Bgfx.SetTransform(pMdl);
-			}
-
-			Bgfx.SetRenderState(RenderState.ColorWrite | RenderState.AlphaWrite | RenderState.BlendAlpha | RenderState.NoCulling);
-			Bgfx.SetVertexBuffer(0, tvb.vertexBuffer, 0, tvb.vidx);
-			Bgfx.Submit(VIEW_ID, colorShader);
+			ViewHelper.Submit(VIEW_ID, Sheds.Color, tvb);
 		}
     }
 }
