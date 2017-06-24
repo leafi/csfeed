5c1a4cf8de7459cfab7f9a41e5db91192c666549
leafi <leaf@leaf.sx>
2017-06-25 00:12:32 +0100

    However minor, more RUI

---
+++ RetainedUI/Label.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Csfeed.RetainedUI
+{
+    public class Label : Component
+    {
+		public string Text = "label text not set";
+
+		public override void Layout()
+		{
+			var sz = Painter.MeasureString(Fonts.UIStandard, Text);
+			W = sz.X;
+			H = sz.Y;
+		}
+
+		public override void Render(Painter painter)
+		{
+			painter.DrawString(X, Y, Fonts.UIStandard, Text, Theme.Global.TextColor);
+		}
+	}
+}
--- RetainedUI/Multi.cs
+++ RetainedUI/Multi.cs
@@ -3,43 +3,16 @@ using System.Collections.Generic;
 
 namespace Csfeed.RetainedUI
 {
-	public enum Orientation
-	{
-		Horizontal,
-		Vertical
-	}
-
 	public class Multi : Component
 	{
 		public List<Component> Children = new List<Component>();
-		public Orientation Orientation = Orientation.Vertical;
 
 		public override bool ResizableW { get { return false; } }
 		public override bool ResizableH { get { return false; } }
 
 		public override void Layout()
 		{
-			int accumPri = 0;
-			int maxSnd = 0;
-
 			RUI.PrepChildren(this, Children);
-
-			foreach (var com in Children) {
-				if (Orientation == Orientation.Horizontal) {
-					com.X = X + accumPri;
-					com.Y = Y;
-					accumPri += com.W;
-					maxSnd = (maxSnd < com.H) ? com.H : maxSnd;
-				} else if (Orientation == Orientation.Vertical) {
-					com.X = X;
-					com.Y = Y + accumPri;
-					accumPri += com.H;
-					maxSnd = (maxSnd < com.W) ? com.W : maxSnd;
-				}
-			}
-
-			W = (Orientation == Orientation.Horizontal) ? accumPri : maxSnd;
-			H = (Orientation == Orientation.Horizontal) ? maxSnd : accumPri;
 		}
 
 		public override void Render(Painter painter)
